0184388e90f14e0546ee560ddff916adecc47fea
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-05-24 23:46:43 +0600

    3 table create complete

---
+++ PostgreSQL_Assignment.sql
@@ -0,0 +1,53 @@
+-- Table Create 
+
+-- rangers table start--
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    region VARCHAR(60)
+);
+
+ drop Table rangers
+
+INSERT INTO rangers (name, region)
+VALUES
+('Alice Green', 'Northern Hills'),
+('Bob White', 'River Delta'),
+('Carol King', 'Mountain Range');
+
+-- rangers table end--
+
+-- species table start--
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50),
+    scientific_name VARCHAR(35),
+    discovery_date DATE,
+    conservation_status VARCHAR(30)
+);
+
+-- species table end--
+
+-- sightings table start--
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INTEGER REFERENCES species(species_id),
+    ranger_id INTEGER REFERENCES rangers(ranger_id),
+    location VARCHAR(100),
+    sighting_time TIMESTAMP,
+    notes TEXT
+);
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes)
+VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+
+-- sightings table end--
+
+-- Problem Solve-1---
